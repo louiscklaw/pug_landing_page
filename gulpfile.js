2e96cda5348d6f77780dd78e44d954883383ffc7
louiscklaw <louiscklaw@gmail.com>
2019-08-18 15:47:46 +0800

    update,

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 // gulpfile.js
 const path = require( 'path' );
 const gulp = require( 'gulp' );
-const { series, parallel } = require('gulp');
-const { exec } = require('child_process');
+const {
+    series,
+    parallel
+} = require( 'gulp' );
+const {
+    exec
+} = require( 'child_process' );
 
 const pug = require( 'gulp-pug' );
 const sass = require( 'gulp-sass' );
@@ -11,8 +16,8 @@ APP_DIR = path.join( __dirname, 'app' );
 CLIENT_DIR = path.join( APP_DIR, 'client' );
 CLIENT_SRC = path.join( CLIENT_DIR, 'src' );
 PUG_INC = path.join( CLIENT_SRC, 'inc' );
-PUG_PATHS = [CLIENT_SRC, PUG_INC];
-PUG_FILEMASK = PUG_PATHS.map( p => path.join( p, '*.pug' ));
+PUG_PATHS = [ CLIENT_SRC, PUG_INC ];
+PUG_FILEMASK = PUG_PATHS.map( p => path.join( p, '*.pug' ) );
 INDEX_PUG = path.join( CLIENT_SRC, 'index.pug' );
 
 SCSS_SRC = path.join( CLIENT_SRC, 'scss' );
@@ -23,10 +28,10 @@ PUBLIC_JS = path.join( PUBLIC_PATH, 'js' );
 
 
 function clean() {
-    return exec('rm -rf ./docs');
+    return exec( 'rm -rf ./docs' );
 }
 
-function re_create () {
+function re_create() {
     return exec( 'mkdir ./docs' );
 }
 
@@ -38,14 +43,14 @@ async function buildHTML() {
     // console.log( "helloworld" );
 };
 
-async function helloworld () {
+async function helloworld() {
     console.log( "helloworld" );
 }
 
 async function sass_compile() {
-  return gulp.src( path.join(SCSS_SRC,'landing-page.scss') )
-    .pipe( sass().on( 'error', sass.logError ) )
-    .pipe( gulp.dest( PUBLIC_CSS ) );
+    return gulp.src( path.join( SCSS_SRC, 'landing-page.scss' ) )
+        .pipe( sass().on( 'error', sass.logError ) )
+        .pipe( gulp.dest( PUBLIC_CSS ) );
 }
 
 
