a19f69e4fd4d213e7f3d5189c950347c720d92bf
louiscklaw <louiscklaw@gmail.com>
2019-08-18 15:52:33 +0800

    update,

--- gulpfile.js
+++ gulpfile.js
@@ -12,27 +12,34 @@ const {
 const pug = require( 'gulp-pug' );
 const sass = require( 'gulp-sass' );
 
+// client souce
 APP_DIR = path.join( __dirname, 'app' );
 CLIENT_DIR = path.join( APP_DIR, 'client' );
+
 CLIENT_SRC = path.join( CLIENT_DIR, 'src' );
 PUG_INC = path.join( CLIENT_SRC, 'inc' );
 PUG_PATHS = [ CLIENT_SRC, PUG_INC ];
-PUG_FILEMASK = PUG_PATHS.map( p => path.join( p, '*.pug' ) );
-INDEX_PUG = path.join( CLIENT_SRC, 'index.pug' );
-
 SCSS_SRC = path.join( CLIENT_SRC, 'scss' );
+CLIENT_IMG = path.join( CLIENT_DIR, 'img' );
 
+// asset folder
 PUBLIC_PATH = path.join( __dirname, 'docs' );
 PUBLIC_CSS = path.join( PUBLIC_PATH, 'css' );
 PUBLIC_JS = path.join( PUBLIC_PATH, 'js' );
+PUBLIC_IMG = path.join( PUBLIC_PATH, 'img' );
 
+INDEX_PUG = path.join( CLIENT_SRC, 'index.pug' );
+PUG_FILEMASK = PUG_PATHS.map( p => path.join( p, '*.pug' ) );
 
 function clean() {
     return exec( 'rm -rf ./docs' );
 }
 
-function re_create() {
-    return exec( 'mkdir ./docs' );
+function copy_img () {
+    copy_cmd = 'cp -r %s %s', CLIENT_IMG, PUBLIC_IMG;
+    console.log( copy_img );
+
+    // return exec(  );
 }
 
 async function buildHTML() {
@@ -56,6 +63,6 @@ async function sass_compile() {
 
 exports.default = series( clean, buildHTML );
 exports.w = () => {
-    gulp.watch( PUG_FILEMASK, series( clean, buildHTML ) );
+    gulp.watch( PUG_FILEMASK, series( clean, buildHTML, copy_img ) );
     gulp.watch( path.join( SCSS_SRC, 'landing-page.scss' ), series( sass_compile ) );
 }
